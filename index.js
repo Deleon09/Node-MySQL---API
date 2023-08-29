afc5afe7b6b79656554f02cda9e80092102c281e
Alberto <alberto101481@gmail.com>
2023-08-29 11:52:50 -0600

    :tada: ADD: Project start

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/employees', (req, res) => res.send('Getting employees'));
+
+app.post('/employees', (req, res) => res.send('Creating employees'));
+
+app.put('/employees', (req, res) => res.send('Updating employees'));
+
+app.delete('/employees', (req, res) => res.send('Deleting employees'));
+
+app.listen(3000);
+console.log('Server on port', 3000);
