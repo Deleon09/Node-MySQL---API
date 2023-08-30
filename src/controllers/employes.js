30217113d9c717cc98a9e28617eba70e6c3c7ca0
Alberto <alberto101481@gmail.com>
2023-08-29 21:58:44 -0600

    :sparkles: ADD: get single employee and delete employee

--- src/controllers/employes.js
+++ src/controllers/employes.js
@@ -6,6 +6,16 @@ export const getEmployes = async(req, res) => {
     res.status(200).json(rows);
 };
 
+export const getEmploye = async(req, res) => {
+    const { id } = req.params;
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
+
+    if(rows.length <= 0)
+        return res.status(404).json({ message: `Employe with id ${id} does not exists` });
+
+    res.status(200).json(rows[0]);
+};
+
 export const createEmployes = async(req, res) => {
     const { name, salary } = req.body;
     const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary]);
@@ -16,6 +26,14 @@ export const createEmployes = async(req, res) => {
     });
 };
 
-export const updateEmployes = (req, res) => res.send('Updating employees');
+export const deleteEmployes = async(req, res) => {
+    const { id } = req.params;
+    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [id]);
 
-export const deleteEmployes = (req, res) => res.send('Deleting employees');
+    if(result.affectedRows <= 0)
+        return res.status(404).json({ message: `Employe with id ${id} does not exists` });
+
+    res.status(200).json({ message: `Employe with id ${id} deleted successfully` });
+};
+
+export const updateEmployes = (req, res) => res.send('Updating employees');
--- src/routes/employes.js
+++ src/routes/employes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { createEmployes, deleteEmployes, getEmployes, updateEmployes } from "../controllers/employes.js";
+import { createEmployes, deleteEmployes, getEmployes, getEmploye, updateEmployes } from "../controllers/employes.js";
 
 const router = Router();
 
 router.get('/employees', getEmployes);
 
+router.get('/employees/:id', getEmploye);
+
 router.post('/employees', createEmployes);
 
 router.put('/employees', updateEmployes);
 
-router.delete('/employees', deleteEmployes);
+router.delete('/employees/:id', deleteEmployes);
 
 export default router;
